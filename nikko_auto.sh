3576cfaf5ab0ec0475ac5435b5cb86f1d16dc829
NikkoRod <43006645+NikkoRod@users.noreply.github.com>
2019-05-14 12:57:25 +0800

    Update nikko_auto.sh

--- nikko_auto.sh
+++ nikko_auto.sh
@@ -24,6 +24,35 @@ service webmin restart
 wget https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh
 
 
+
+#Install Nginx, inadd ko nlang den nginx para s client.ovpn
+yum -y install nginx php-fpm php-cli
+service nginx restart
+service php-fpm restart
+chkconfig nginx on
+chkconfig php-fpm on
+service nginx restart
+service php-fpm restart
+
+# Server
+cd
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/NikkoRod/sample/master/nginx.conf"
+sed -i 's/www-data/nginx/g' /etc/nginx/nginx.conf
+mkdir -p /home/vps/public_html
+echo "<pre>Modify nyo nlang hehehe ~Nikkorod</pre>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+rm /etc/nginx/conf.d/*
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/NikkoRod/sample/master/vps.conf"
+sed -i 's/apache/nginx/g' /etc/php-fpm.d/www.conf
+chmod -R +rx /home/vps
+service php-fpm restart
+service nginx restart
+cp client.ovpn /home/vps/public_html/
+chown -R nginx:nginx /home/vps/public_html
+
+
+
+
 #ok done kau n bahala s iba
 
 
